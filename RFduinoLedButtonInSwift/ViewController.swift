c7bc377e95b5584c1dc2f8e79b08ced812be8f59
Cristian Duguet <cristian@cristian-mac.local>
2015-09-15 16:45:07 -0300

    Initial Commit

---
+++ RFduinoLedButtonInSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RFduinoLedButtonInSwift
+//
+//  Created by Cristian Duguet on 9/15/15.
+//  Copyright (c) 2015 TrainFES. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
